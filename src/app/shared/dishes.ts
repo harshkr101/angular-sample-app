9e1f1b3a48fa54b681c4064612a812c75d2cedf0
harshcto <harshcto@gmail.com>
2019-08-28 17:01:09 +0530

    image changes

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -3,14 +3,14 @@ import { Dish } from './dish';
 export const DISHES: Dish[] = [
     {
         id: '0',
-        name: 'Uthappizza',
-        image: '/assets/images/uthappizza.png',
+        name: 'Pizza',
+        image: '/assets/images/pizza.png',
         category: 'mains',
         featured: true,
         label: 'Hot',
-        price: '4.99',
+        price: '12.99',
         // tslint:disable-next-line:max-line-length
-        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        description: ' Italian pizza topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
         comments: [
             {
                 rating: 5,
@@ -46,13 +46,13 @@ export const DISHES: Dish[] = [
     },
     {
         id: '1',
-        name: 'Zucchipakoda',
-        image: '/assets/images/zucchipakoda.png',
+        name: 'Pasta',
+        image: '/assets/images/pasta.png',
         category: 'appetizer',
         featured: false,
         label: '',
-        price: '1.99',
-        description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+        price: '6.99',
+        description: 'Well garnished italian  pasta ',
         comments: [
             {
                 rating: 5,
@@ -88,13 +88,13 @@ export const DISHES: Dish[] = [
     },
     {
         id: '2',
-        name: 'Vadonut',
-        image: '/assets/images/vadonut.png',
-        category: 'appetizer',
+        name: 'Donut',
+        image: '/assets/images/donut.png',
+        category: 'dessert',
         featured: false,
         label: 'New',
-        price: '1.99',
-        description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+        price: '3.99',
+        description: 'Best American donuts you get in world',
         comments: [
             {
                 rating: 5,
@@ -130,13 +130,13 @@ export const DISHES: Dish[] = [
     },
     {
         id: '3',
-        name: 'ElaiCheese Cake',
-        image: '/assets/images/elaicheesecake.png',
+        name: 'Choclate pastry',
+        image: '/assets/images/pastry.png',
         category: 'dessert',
         featured: false,
         label: '',
-        price: '2.99',
-        description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+        price: '3.99',
+        description: 'A delectable, sweet New York Style Choclate Pastry',
         comments: [
             {
                 rating: 5,
