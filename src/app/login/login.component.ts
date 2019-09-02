3ce536d1e686bde7ab8058b63b32af866a2c8281
harshcto <harshcto@gmail.com>
2019-09-02 14:05:31 +0530

    form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
   user =  {
     username: '',
     password: '',
-    remeber:  false
+    remember:  false
   };
 
   constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
